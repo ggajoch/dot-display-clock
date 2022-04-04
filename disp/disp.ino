ff3ea98614dc7434f57bd6495ff920a5134bb648
buleks <unknown>
2022-04-04 19:45:00 +0200

    NTp update with standard library, DTC seems working

--- disp/disp.ino
+++ disp/disp.ino
@@ -2,8 +2,13 @@
 // ----------------- CONFIG -----------------
 // ------------------------------------------
 
-const char* ssid     = "ssid";
-const char* password = "pass";
+//BOARD- NodeMCU 1.0
+//ntp description https://werner.rothschopf.net/202011_arduino_esp8266_ntp_en.htm
+//time docs https://github.com/esp8266/Arduino/blob/master/cores/esp8266/time.cpp
+
+
+const char* ssid     = "xx";
+const char* password = "xxx";
 
 byte latchPin = 5;
 byte enablePin = 16;
@@ -20,90 +25,23 @@ byte sckPin = 4;
 
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-
 #include "SPI.h"
 #include "font-5x7.h"
 #include "SoftwareSPI.h"
 
-#if not __AVR__
-
-enum    _MONTHS_ { 
-  JANUARY, FEBRUARY, MARCH, APRIL, 
-  MAY, JUNE, JULY, AUGUST, 
-  SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER 
-};
-
-enum    _WEEK_DAYS_ { 
-  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, 
-  THURSDAY, FRIDAY, SATURDAY 
-};
-
-#define   ONE_HOUR   3600
-
-#endif 
-
-
-int eu_dst(const time_t * timer) {
-    struct tm       tmptr;
-    uint8_t         month, mday, hour, day_of_week, d;
-    int             n;
-
-    /* obtain the variables */
-                    gmtime_r(timer, &tmptr);
-                    month = tmptr.tm_mon;
-                    day_of_week = tmptr.tm_wday;
-                    mday = tmptr.tm_mday - 1;
-                    hour = tmptr.tm_hour;
-
-    if              ((month > MARCH) && (month < OCTOBER))
-                        return ONE_HOUR;
-
-    if              (month < MARCH)
-                        return 0;
-    if              (month > OCTOBER)
-                        return 0;
-
-    /* determine mday of last Sunday */
-                    n = tmptr.tm_mday - 1;
-                    n -= day_of_week;
-                    n += 7;
-                    d = n % 7;  /* date of first Sunday */
-
-                    n = 31 - d;
-                    n /= 7; /* number of Sundays left in the month */
-
-                    d = d + 7 * n;  /* mday of final Sunday */
-
-    if              (month == MARCH) {
-        if (d < mday)
-            return 0;
-        if (d > mday)
-            return ONE_HOUR;
-        if (hour < 1)
-            return 0;
-        return ONE_HOUR;
-    }
-    if              (d < mday)
-                        return ONE_HOUR;
-    if              (d > mday)
-                        return 0;
-    if              (hour < 1)
-                        return ONE_HOUR;
-                    return 0;
-
-}
 
 SoftSPI SSPI(mosiPin, sckPin);
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 IPAddress timeServerIP;
 
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+const char* NTPServerName = "pool.ntp.org";
 
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP UDP;
+#define TIME_ZONE "CET-1CEST,M3.5.0/02,M10.5.0/03" // https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+
+
+time_t timeUNIX = 0;
+tm tm; 
 
 constexpr uint8_t COLUMNS = 28;
 constexpr uint8_t ROWS = 7;
@@ -159,60 +97,48 @@ uint8_t rows_clear_to_address[ROWS] = {
   0b10001
 };
 
-const char* NTPServerName = "pool.ntp.org";
-byte NTPBuffer[NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
-
-uint32_t getTime() {
-  if (UDP.parsePacket() == 0) { // If there's no response (yet)
-    return 0;
-  }
-  UDP.read(NTPBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-  // Combine the 4 timestamp bytes into one 32-bit number
-  uint32_t NTPTime = (NTPBuffer[40] << 24) | (NTPBuffer[41] << 16) | (NTPBuffer[42] << 8) | NTPBuffer[43];
-  // Convert NTP time to a UNIX timestamp:
-  // Unix time starts on Jan 1 1970. That's 2208988800 seconds in NTP time:
-  const uint32_t seventyYears = 2208988800UL;
-  // subtract seventy years:
-  uint32_t UNIXTime = NTPTime - seventyYears;
-  return UNIXTime;
+inline int getSeconds() {
+  return tm.tm_sec;
 }
 
-void sendNTPpacket(IPAddress& address) {
-  memset(NTPBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
-  // Initialize values needed to form NTP request
-  NTPBuffer[0] = 0b11100011;   // LI, Version, Mode
-  // send a packet requesting a timestamp:
-  UDP.beginPacket(address, 123); // NTP requests are to port 123
-  UDP.write(NTPBuffer, NTP_PACKET_SIZE);
-  UDP.endPacket();
+inline int getMinutes() {
+  return tm.tm_min;
 }
 
-inline int getSeconds(uint32_t UNIXTime) {
-  return UNIXTime % 60;
+inline int getHours() {
+  return tm.tm_hour;
 }
 
-inline int getMinutes(uint32_t UNIXTime) {
-  return UNIXTime / 60 % 60;
-}
 
-inline int getHours(uint32_t UNIXTime) {
-  return UNIXTime / 3600 % 24;
-}
-
-void startUDP() {
-  Serial.println("Starting UDP");
-  UDP.begin(123);                          // Start listening for UDP messages on port 123
-  Serial.print("Local port:\t");
-  Serial.println(UDP.localPort());
+void showTime() {
+  Serial.print("year:");
+  Serial.print(tm.tm_year + 1900);  // years since 1900
+  Serial.print("\tmonth:");
+  Serial.print(tm.tm_mon + 1);      // January = 0 (!)
+  Serial.print("\tday:");
+  Serial.print(tm.tm_mday);         // day of month
+  Serial.print("\thour:");
+  Serial.print(tm.tm_hour);         // hours since midnight  0-23
+  Serial.print("\tmin:");
+  Serial.print(tm.tm_min);          // minutes after the hour  0-59
+  Serial.print("\tsec:");
+  Serial.print(tm.tm_sec);          // seconds after the minute  0-61*
+  Serial.print("\twday");
+  Serial.print(tm.tm_wday);         // days since Sunday 0-6
+  if (tm.tm_isdst == 1)             // Daylight Saving Time flag
+    Serial.print("\tDST");
+  else
+    Serial.print("\tstandard");
   Serial.println();
 }
-
 void setup() {
 
   Serial.begin(115200);
 
   Serial.println("uruchomiono kaktus...");
-  
+
+
+  configTime(TIME_ZONE, NTPServerName); 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -227,16 +153,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 
 
-  startUDP();
-
-  if(!WiFi.hostByName(NTPServerName, timeServerIP)) { // Get the IP address of the NTP server
-    Serial.println("DNS lookup failed. Rebooting.");
-    Serial.flush();
-    ESP.reset();
-  }
-  Serial.print("Time server IP:\t");
-  Serial.println(timeServerIP);
-
+  time(&timeUNIX); 
+  showTime();
 
   pinMode(enablePin, OUTPUT);
   digitalWrite(enablePin, HIGH);
@@ -263,7 +181,7 @@ void setup() {
 
   delay(1000);
   
-  print_unix_timestamp_utc(1547930250);
+  print_unix_timestamp(1547930250);
 }
 
 void send_bytes(uint8_t byteRows, uint8_t byteColumns) {
@@ -358,60 +276,49 @@ void print_hour(int h, int m, int s)
 }
 
 
-void print_unix_timestamp(const long int time) {
-    print_hour(getHours(time), getMinutes(time), getSeconds(time));
+void print_unix_timestamp(time_t time) 
+{
+    localtime_r(&time, &tm); 
+    print_hour(getHours(), getMinutes(), getSeconds());
 }
-
-
-void print_unix_timestamp_utc(long int time) {
-    auto diff = eu_dst(&time);
-    time += diff;
-    time += ONE_HOUR;
-    print_hour(getHours(time), getMinutes(time), getSeconds(time));
-}
-
-
 
 
 unsigned long intervalNTP = 60000; // Request NTP time every minute
 unsigned long prevNTP = 0;
 unsigned long lastNTPResponse = millis();
-uint32_t timeUNIX = 0;
-
 unsigned long prevActualTime = 0;
 
 
-byte initialized = 0;
 
 void loop()
 {
     unsigned long currentMillis = millis();
-
-    if (currentMillis - prevNTP > intervalNTP || initialized == 0) { // If a minute has passed since last NTP request
+    
+    if (currentMillis - prevNTP > intervalNTP) { // If a minute has passed since last NTP request
       prevNTP = currentMillis;
-      Serial.println("\r\nSending NTP request ...");
-      sendNTPpacket(timeServerIP);               // Send an NTP request
-      initialized = 1;
-    }
-
-    uint32_t time = getTime();                   // Check if an NTP response has arrived and get the (UNIX) time
-    if (time) {                                  // If a new timestamp has been received
-      timeUNIX = time;
-      Serial.print("NTP response:\t");
-      Serial.println(timeUNIX);
+      Serial.println("Updating NTP time");
+      Serial.flush();
+      time(&timeUNIX); 
+      localtime_r(&timeUNIX, &tm);           // update the structure tm with the current time
+      showTime();
       lastNTPResponse = currentMillis;
-    } else if ((currentMillis - lastNTPResponse) > 3600000) {
+      
+    }
+      
+    if ((currentMillis - lastNTPResponse) > 3600000) 
+    {
       Serial.println("More than 1 hour since last NTP response. Rebooting.");
       Serial.flush();
       ESP.reset();
     }
 
 
-    uint32_t actualTime = timeUNIX + (currentMillis - lastNTPResponse)/1000;
+    time_t actualTime = timeUNIX + (currentMillis - lastNTPResponse)/1000;
     if (actualTime != prevActualTime && timeUNIX != 0) { // If a second has passed since last print
       prevActualTime = actualTime;
-      Serial.printf("\rUTC time:\t%02d:%02d:%02d   \r\n", getHours(actualTime), getMinutes(actualTime), getSeconds(actualTime));
-      print_unix_timestamp_utc(actualTime);
+      localtime_r(&actualTime, &tm);  
+      Serial.printf("\rUTC time:\t%02d:%02d:%02d   \r\n", getHours(), getMinutes(), getSeconds());
+      print_unix_timestamp(actualTime);
     }
 
 }
